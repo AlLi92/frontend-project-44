9115b243e7957b5e3051c0f7378944191dbaf4d7
AlLi92 <yelle34@yandex.ru>
2024-05-04 19:59:31 +0300

    task 7 completed

---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+import runGame from "../index.js";
+import getRandomInt from "../random.js";
+
+const task = 'Find the greatest common divisor of given numbers.';
+
+const getGсd = (num1, num2) => {
+    if (num2 > num1) return getGсd(num2, num1);
+    if (!num2) return num1;
+    return getGсd(num2, num1 % num2);
+  };
+  
+  const gcdQuestion = () => {
+    const num1 = getRandomInt(1, 99);
+    const num2 = getRandomInt(1, 99);
+    const question = `${num1} ${num2}`;
+  
+    const answer = String(getGсd(num1, num2));
+  
+    return { question, answer };
+  };
+  
+  export default () => runGame(task, gcdQuestion);
